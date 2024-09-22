6ef9a645ba17e0f490e5291b63e20031b40be1d6
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2024-09-22 19:45:22 +0930

    Add files via upload

---
+++ question12.sh
@@ -0,0 +1,4 @@
+git checkout branch2
+git stash pop
+git add file4
+git commit -m "restore and commit the changes of file4"
---
+++ question13.sh
@@ -0,0 +1,6 @@
+git checkout -b new-branch
+rm -rf *.sh
+touch file13.txt
+git add file13.txt
+git commit -m "add file13"
+git push origin new-branch
